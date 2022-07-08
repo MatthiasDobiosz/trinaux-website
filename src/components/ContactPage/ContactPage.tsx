e201c0e151d9a71101a28e670e163eead15f8240
MatthiasDobiosz <matthido98@web.de>
2022-07-08 16:58:03 +0200

    added initial contact form

--- src/components/ContactPage/ContactPage.tsx
+++ src/components/ContactPage/ContactPage.tsx
@@ -1,15 +1,20 @@
 import React from "react";
 import ContactMap from "./ContactMap";
 import "./ContactPage.css";
+import InputForm from "./InputForm";
 
 const ContactPage = () => {
     return(
-        <div className="contact-page-container">  
-            <div className="map-container">
-                <ContactMap />
-            </div>            
-            <div className="inputForm-container">
-            </div>
+        <div>
+            <h1 className="contact-page-header"> Contact Us! </h1>
+            <div className="contact-page-container">  
+                <div className="map-container">
+                    <ContactMap />
+                </div>            
+                <div className="inputForm-container">
+                    <InputForm/>
+                </div>
+        </div>
        </div>
         
     );
--- src/components/ContactPage/InputForm.tsx
+++ src/components/ContactPage/InputForm.tsx
@@ -3,8 +3,26 @@ import "./InputForm.css";
 
 const InputForm = () => {
     return(
-        <>
-        </>
+        <div className="Input-Form">
+            <h2 className="Input-header">Schreibe uns ganz einfach eine Mail <br/> oder ruf uns an!</h2>
+            <form className="email-form">
+                <div className="form-first"> 
+                    <label htmlFor="fname">First Name:</label><br/>
+                    <input type="text" id="fname" name="fname"/><br/>
+                    <label htmlFor="lname">Last Name:</label><br/>
+                    <input type="text" id="lname" name="lname"/><br/>
+                    <label htmlFor="email">Email:</label><br/>
+                    <input type="text" id="email" name="email"/>
+                </div>
+                <div className="form-second">
+                    <label htmlFor="etext">Ýour Text:</label><br/>
+                    <input type="text" id="etext" name="etext"/>
+                </div>
+                <div className="form-third">                
+                    <input className="submit-btn" type="submit" value="Send Email!"/>               
+                </div>
+            </form>
+        </div>
     );
 }
 
